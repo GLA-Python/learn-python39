30b777ef36493da007173b3f2d8a6088fcc2283f
amirkhan1092 <amirkhan1092@gmail.com>
2021-03-09 15:48:31 +0530

    hackerrank contest python39-c1 check divisibility

---
+++ hacherrank_python39/divisibility_check.py
@@ -0,0 +1,8 @@
+num = int(input())  # number
+
+d1 = int(input())  # first divisor
+d2 = int(input())  # second divisor
+if num % d1 == 0 and num %d2 == 0:
+    print("Yes.")
+else:
+    print("No.")
