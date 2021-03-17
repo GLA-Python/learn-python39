784f94ab585cd9606242f686a2a4771dad52d8b0
amirkhan1092 <amirkhan1092@gmail.com>
2021-03-17 09:02:21 +0530

    char identity selection

---
+++ learn-python39/char_identity.py
@@ -0,0 +1,9 @@
+ch = input()
+if ch >= 'A' and ch <= 'Z':
+    print("Upper case alphabet")
+elif(ch>='a' and ch<='z'):
+    print("lower case ")
+elif(ch>='0' and ch<='9'):
+    print("digit")
+else:
+    print("special character")
