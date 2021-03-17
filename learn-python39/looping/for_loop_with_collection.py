5b42fa2907158e74b07f7cfd9a5be9275758d4e3
amirkhan1092 <amirkhan1092@gmail.com>
2021-03-17 09:01:49 +0530

    for loop with collections

---
+++ learn-python39/looping/for_loop_with_collection.py
@@ -0,0 +1,49 @@
+# for loop with list
+lst = [2, 33]
+for i in lst:
+    print(i)
+    lst[1] = 'Hi'
+# Output
+# 2
+# Hi
+
+
+# for loop with tuple
+tp = (2, 43, 'hello')
+for i in tp:
+    print(i)
+# output
+# 2
+# 43
+# hello
+
+
+# for loop with string
+st = 'Python'
+for i in st:
+    print(i)
+# Output
+# P
+# y
+# t
+# h
+# o
+# n
+
+# for loop with range(start, stop, step)
+for i in range(1, 10, 2):
+    print(i)
+'''
+Output
+1
+3
+5
+7
+9
+'''
+
+
+
+
+
+
