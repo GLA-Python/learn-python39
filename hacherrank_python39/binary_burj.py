80769625d7305a629115d09a2133dd39448f69ac
amirkhan1092 <amirkhan1092@gmail.com>
2021-03-21 01:33:30 +0530

    binary burj pattern

---
+++ hacherrank_python39/binary_burj.py
@@ -0,0 +1,18 @@
+'''
+binary burj
+   1
+  10
+  11
+ 100
+ 101
+ 110
+ 111
+1000
+1001
+1010
+'''
+
+num = int(input())
+ln = len(bin(num)) - 2
+for i in range(num+1):
+    print(f'{{0:>{ln}}}'.format(bin(i)[2:]))
