a70641b26dbf19cdb2cc55c0b3773e2430bb8f58
amirkhan1092 <amirkhan1092@gmail.com>
2021-04-09 10:34:03 +0530

    traverse string (range iteration)

---
+++ learn-python39/string_basics/traverse_string.py
@@ -0,0 +1,14 @@
+"""
+iterating  over the string using for loop
+traversing
+"""
+# using iteration
+st = 'python programming'
+for ch in st:
+    print(ch, end=' ')
+
+# using range function
+st = 'Python Programming'
+ln = len(st)
+for i in range(ln):
+    print(st[i], end=' ')
