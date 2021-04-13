95b5d1eeb16806ee8da153239d2ccc6037033b97
amirkhan1092 <amirkhan1092@gmail.com>
2021-04-13 11:41:58 +0530

    for loop updated

--- learn-python39/looping/for_loop_structure.py
+++ learn-python39/looping/for_loop_structure.py
@@ -1,5 +1,4 @@
 # loop
-
 # 1. for loop
 # 2. while loop
 
@@ -9,7 +8,22 @@
 for index in sequential_entity:
     # for body
 '''
+'''
+C- Programming : for loop work with comparison 
+for(int i=0; i<100; i++)
+    {
+        printf("Hello");
+        i = 99;
+    }
+Python3 : for loop work with assignment 
+for i in [0, 1, 2, 3, 4, 5]:
+    print("Hello", i)  
+    i = 99  
+    
+'''
 
+
+# example sum of list
 s = 0
 for i in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
     s = s + i
@@ -18,18 +32,10 @@ else:
     print('loop completed')
 print(f'natural sum is {s}')
 
-
-
-
-
-
-
-
-
-
-
-
-
+# double index in for loop
+x = [[1,2], [2, 3], [3, 3], 'Hi']
+for i, j in x:  # i, j = x[0]
+    print(i)
 
 
 c=0
@@ -44,8 +50,8 @@ else:
 
 
 
-
-
+# else part with for loop
+# home work
 
 
 
