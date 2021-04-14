bb91737b3b53dd7286659664b81a0110f716284f
amirkhan1092 <amirkhan1092@gmail.com>
2021-04-14 05:55:38 +0530

    assignment problem 1 solution

--- Python_assignments_sol/Assignment1/a_girlfriend.py
+++ Python_assignments_sol/Assignment1/a_girlfriend.py
@@ -1,7 +1,10 @@
 """
 distance cover with steps 1, 2, 3, 4, or 5
 """
-
+from math import ceil
 cor = int(input('Coordinate of girl friends house\' '))
 steps = int((cor + 4) / 5)  # logic 1
+steps = (cor + 4) // 5  # logic 2
+steps = ceil(cor / 5)  # logic 3
+
 print(steps)
