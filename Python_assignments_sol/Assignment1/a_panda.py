8672944044e16bed2c73ff6506a3e568d39783d2
amirkhan1092 <amirkhan1092@gmail.com>
2021-04-14 07:27:06 +0530

    assignment problem 2 solution

--- Python_assignments_sol/Assignment1/a_panda.py
+++ Python_assignments_sol/Assignment1/a_panda.py
@@ -10,9 +10,30 @@ from math import ceil
 pieces = int(input('Enter the number of pieces '))
 cake_bars = input(f'Enter the {pieces} integers ')
 
-# alternate solution
+# alternate solution: some cases failed
 value = str(int(cake_bars.replace(' ', '')))
 ways = ceil(int(value, 2) / len(value))
 print(ways)
 
 # main solution using list and loop
+cake_bars = list(map(int, cake_bars.split()))
+count = 0
+sum = 1
+i = 0
+while i < pieces:
+    count = 0
+    if cake_bars[i]:
+        i += 1
+        while i < pieces:
+            if cake_bars[i]:
+                sum *= count + 1
+                break
+            count += 1
+            i += 1
+            # print(count)
+        else:
+            if cake_bars[i-1]:
+                sum *= count+1
+    else:
+        i += 1
+print(sum)
