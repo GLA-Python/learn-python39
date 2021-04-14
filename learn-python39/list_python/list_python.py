64316c15056893d6a90372e06cd4b7225986e26b
amirkhan1092 <amirkhan1092@gmail.com>
2021-04-14 11:08:46 +0530

    list methods

--- learn-python39/list_python/list_python.py
+++ learn-python39/list_python/list_python.py
@@ -70,3 +70,95 @@ lst = [12, 32.8, 22+3j, 'hello', [3, 43]]
 lst.append(100)
 print(lst)
 
+
+# methods in a list class
+# 1. append(): add the element or item at the end of the list
+lst = [2, 43, 'Hello', [3, 5]]  # len 4
+lst.append('Python')  # len 5
+lst.append([2, 4, 54])  # len 6
+lst.append(100) # len 7
+print(lst)
+# Output [2, 43, 'Hello', [3, 5], 'Python', [2, 4, 54], 100]
+
+# 2. extend: add the list or iterable items in a current list
+lst = [2, 43, 'Hello', [3, 5]]  # len 4
+lst.extend([100, 120, 130])  # len 4 + 3 = 7
+# lst.extend(100)  # generate error 'int' object is not iterable
+lst.extend('Hello')
+print(lst)
+
+# 3. insert : add the element at position
+lst = [2, 43, 'Hello', [3, 5]]
+lst.insert(0, 'Python')
+print(lst)
+
+
+# 4. clear : remove all the items in a list
+lst = [2, 43, 100, 'hello']
+lst.clear()
+print(lst)
+
+# 5. pop(): remove the element at specified index
+lst = [2, 4, 34, 100, [2, 43]]
+ele = lst.pop(3)  # remove the item at given index 3 and return value 100
+print(f'element {ele}  removed', lst)
+
+# 6. remove: remove the element at specified value
+lst = [2, 4, 34, 100, [2, 43]]
+rm = [2, 34, 100]
+for i in rm:
+    lst.remove(i)
+print(lst)
+
+
+# index(): return the index of element
+lst = [2, 4, 34, 100, [2, 43]]
+re = lst.index(2)
+print(re)  # 0
+
+# count() : count the frequency of the element
+lst = [2, 4, 34, 100, [2, 43], 100, 2]
+re = lst.count(100)
+print(re)  # 2
+
+# 9. sort : sort the list
+# example 1
+lst = [2, 4, 34, 10, 100, 2, 0 ]
+print(f'before sorting {lst}')
+lst.sort()
+print(f'after sorting {lst}')
+
+# example 2
+lst = ['ravi', 'ram', 'akshay', 'akhil']
+print(f'before sorting {lst}')
+lst.sort(reverse=True)
+print(f'after sorting {lst}')
+
+
+# 10. reverse()
+lst = [2, 4, 100, 'Hi']
+lst.reverse()
+# re = lst[::-1]
+print(lst)
+
+# 11. copy: return the copy of the list
+lst1 = [2, 4, 43, 100]
+lst2 = lst1.copy()  # list class method copy
+# lst2 = lst1[:] alternate method slicing
+lst2.append(100)
+print('lst1', lst1)  # lst1 [2, 4, 43, 100]
+print('lst2', lst2)  # lst2 [2, 4, 43, 100, 100]
+
+
+# sort the list with item length
+# def apna_fun(x):
+#     return  x[-1]
+
+lst = ['ravi', 'ram', 'akshay', 'akhil']
+lst.sort(key=len)
+print(lst)
+
+
+
+
+
