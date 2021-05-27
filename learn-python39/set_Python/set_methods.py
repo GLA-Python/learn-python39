c4ff1f193a1ab71244a381c18707d6f0df43372d
amirkhan1092 <amirkhan1092@gmail.com>
2021-05-27 09:51:53 +0530

    list practice test question

---
+++ learn-python39/set_Python/set_methods.py
@@ -0,0 +1,24 @@
+ls = [3, 4,3, 4]
+v = sorted(ls)
+print(ls)
+
+
+# write a python Program to check given list is sorted or not?
+# 3 5 65 9 3
+# not sorted
+
+# 2 3 65 77
+# sorted
+
+lst = list(map(int, input().split()))
+print('sorted') if sorted(lst) == lst else print('not sorted')
+
+
+a = 1
+b = 21
+v = a if a > 10 else b
+# if a > 10:
+#     v = a
+# else:
+#     v = b
+print(v)
