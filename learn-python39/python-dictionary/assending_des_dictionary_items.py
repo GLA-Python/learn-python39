3b651619739c4e8dd6e6e7da16c622080319fbae
amirkhan1092 <amirkhan1092@gmail.com>
2021-05-27 09:51:25 +0530

    python ascending order

--- learn-python39/python-dictionary/assending_des_dictionary_items.py
+++ learn-python39/python-dictionary/assending_des_dictionary_items.py
@@ -10,5 +10,6 @@ itm = list(dct.items())
 print(itm)  # before sort  [('amit', 3), ('raj', 2), ('abhi', 4), ('aastha', 3), ('yash', 0)]
 itm.sort(key=khudka_fun)  # assending
 itm.sort(key=khudka_fun, reverse=True)  # dessending order
+
 print(itm)  # after sort
 
