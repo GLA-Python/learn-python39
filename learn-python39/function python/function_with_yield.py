bf928432d60c7b37cc483c7a47949bd797e4b247
amirkhan1092 <amirkhan1092@gmail.com>
2021-06-08 08:53:34 +0530

    function execution with return type

---
+++ learn-python39/function python/function_with_yield.py
@@ -0,0 +1,33 @@
+"""
+we use yield when we need to iterate over the sequence and we dont want to store entire sequence in a memory.
+Yield used in Python generator(generator like a normal function but whenever it needs the value it does)
+"""
+# operations with number
+
+
+def opera(num):
+    return num * 2, num * 3, num * 4  # return statement close the execution of the function with return value in memory
+    print('after return')
+    print(vag)
+
+
+a = 2
+x = opera(a)
+print(x)
+
+# with yield
+
+
+def opera(num):
+    yield num * 2  # add value (num*2) to generator
+    yield num * 3  # add value to generator
+    yield num * 4  # add value to generator
+    # yield doesnt close the execution
+    print('after yield')
+    print(3)
+    yield 20
+    print('in the end')
+a
+a = 2
+x = opera(a)
+print(x)
