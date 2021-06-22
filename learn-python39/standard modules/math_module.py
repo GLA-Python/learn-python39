bcc75dc77d0c0ab80974931197e2fcf661fac880
amirkhan1092 <amirkhan1092@gmail.com>
2021-06-22 11:55:20 +0530

    math modules basics

---
+++ learn-python39/standard modules/math_module.py
@@ -0,0 +1,58 @@
+"""
+use for mathematical function
+"""
+
+
+v = math.sin(100)
+print(v)
+
+
+# sqrt(): to calculate the square root of the given number
+import math
+k = math.sqrt(16)
+print(k)
+
+
+# ceil(): return the smallest integer equal or greater to the number
+
+num = 23.001
+k = math.ceil(num)
+print(k)
+
+
+
+# floor()
+
+num = 23.43
+k = math.floor(num)
+print(k)
+
+
+# log()
+
+v = math.log(8, 2)
+print(v) # 3
+
+
+
+
+# byte size calculation
+k = 65536
+# logic
+bt = int(math.log(k, 256)) + 1
+print(bt)
+
+
+#
+
+
+# square root
+from math import *
+num = 5
+k = sqrt(num)
+ans = factorial(num)
+print(f'Square root of {num} is {k} and factorial {ans}')
+
+
+
+
