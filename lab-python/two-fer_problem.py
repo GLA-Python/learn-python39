c0b56c961ffb614caefd747bbf06c2cd6b424496
amirkhan1092 <amirkhan1092@gmail.com>
2021-07-20 14:55:29 +0530

    python lab sheet

---
+++ lab-python/two-fer_problem.py
@@ -0,0 +1,17 @@
+
+
+'''
+. Function Two-fer or 2-fer is short for two for one. One for you and one for me.
+"One for X, one for me."
+When X is a name or "you".
+If the given name is "Alice", the result should be "One for Alice, one for me." If no name is given, the
+result should be "One for you, one for me."
+def Two-fer():
+return “One for X, one for me."
+'''
+
+def two_fer(x='You'):
+    # logic here 
+    return f'one for {x}, one for Me'
+
+print(two_fer('Rahul'))
