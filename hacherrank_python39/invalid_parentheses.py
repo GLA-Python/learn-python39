92f092e6afcd64170c4f304a07070f004c82d701
amirkhan1092 <amirkhan1092@gmail.com>
2021-07-20 12:05:09 +0530

    Invalid paranthesis

---
+++ hacherrank_python39/invalid_parentheses.py
@@ -0,0 +1,17 @@
+st = input()
+
+count = 0
+
+for i in st:
+    if i == '(':
+        count += 1
+    elif i == ')':
+        count -= 1
+        if count < 0:
+            break           
+
+if count == 0:
+    print("'True'")
+else:
+    print("'False'")
+            
