8bbe96b375be69436acdbe5050eec4cdae582df5
Ashish24thakur <87715030+Ashish24thakur@users.noreply.github.com>
2021-07-21 17:54:36 +0530

    Update binary_burj.py

--- hacherrank_python39/binary_burj.py
+++ hacherrank_python39/binary_burj.py
@@ -19,6 +19,15 @@ for i in range(num+1):
     print(f'{{0:>{w}}}'.format(bin(i)[2:]))
     # x = bin(i)[2:]
     # print(' '*(w-len(x))+x)
+      
+      
+n=int(input())
+for i in range(n+1):
+    b=bin(i).replace("0b","") 
+    for j in range(len(bin(n).replace("0b",'')) - len(b)):
+        print(" ",end='')
+    print(b) 
+
 
 
 
