02ad6a56df51e8dae6244efc4f5cb862e42cc01f
Aryan Upadhyay <73682643+aryan-upa@users.noreply.github.com>
2021-07-26 11:16:50 +0530

    A more mathematical approach with O(1) time.

--- hacherrank_python39/the_war.py
+++ hacherrank_python39/the_war.py
@@ -3,11 +3,5 @@ THE WAR
 
 """
 
-
-r, c = map(int, input().split(' x '))
-count = 0
-for i in range(1, r+1):
-    for j in range(1, c+1):
-        if (i + j) % 2:
-            count += 1
-print(count)            
+r,c = map(int,input().split(' x '))
+print(r*c//2)
